432232f81a1e9b9c938075537fea3fa13202dd45
Emily Joanna Alves <88635006+emilyalvese@users.noreply.github.com>
2023-03-08 15:58:23 -0300

    Update Exercicio1.java

--- lista1/src/lista1/Exercicio1.java
+++ lista1/src/lista1/Exercicio1.java
@@ -1,11 +1,37 @@
 package lista1;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio1 {
-
+	
+	public static void totalPagamento(Double precoLitro, Integer quantidadeLitro) {
+		double result = precoLitro*quantidadeLitro;
+		JOptionPane.showMessageDialog(null, "Preço total: R$"+ result);
+	}
+	
 	public static void main(String[] args) {
+		Double precoLitro = 0.0;
+		Integer quantidadeLitro = 0;
+		
+		String showInputDialog = JOptionPane.showInputDialog("Insira o preço por litro");
 
+		if(showInputDialog.isEmpty()){
+			JOptionPane.showMessageDialog(null, "Nada foi inserido.");
+		}
+		else{
+			precoLitro = Double.valueOf(showInputDialog);
+		}
+
+		String showInputDialog2 = JOptionPane.showInputDialog("Insira o número de litros");
 		
+		if(showInputDialog2.isEmpty()){
+			JOptionPane.showMessageDialog(null, "Nada foi inserido.");
+		}
+		else{
+			quantidadeLitro = Integer.valueOf(showInputDialog2);
+		}
 		
+		totalPagamento(precoLitro, quantidadeLitro);
 	}
 
 }
