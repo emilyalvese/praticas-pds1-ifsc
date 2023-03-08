afbe9c5f248ba30cbee68672cc5773d99ae7effc
Emily Joanna Alves <88635006+emilyalvese@users.noreply.github.com>
2023-03-07 21:09:49 -0300

    Update Teoricas.java

--- lista1/src/lista1/Teoricas.java
+++ lista1/src/lista1/Teoricas.java
@@ -1,7 +1,7 @@
 package lista1;
 
 public class Teoricas {
-
+	
 	// 1. Em programação, o que é uma biblioteca?
 	// R: Uma biblioteca nada mais é do que o código-fonte compilado produzido por terceiros, possibilitando assim a adição de novas funções ao código-fonte do programa.
 
@@ -16,6 +16,15 @@ public class Teoricas {
 	//    e) Botão de Opção =  JOptionPane
 	//    f) Caixa de Combinação = JComboBox
 	
-	// 4. 
+	// 4. Quais são os propósitos dos layouts no Swing? E quais são os layouts possíveis?
+	// R: O principal propósito é a criação de interfaces gráficas e, além disso, a boa distribuição dos componentes na tela, de acordo com o desejo do programador. Os possíveis layout managers são: BorderLayout, BoxLayout, FlowLayout, GridLayout
 	
+	// 5. Como é possível fazer a validação de entrada de dados em campos de texto?
+	// R: Usando uma if clause com a condição de validação logo após a leitura do texto e seu respecito armazenamento em uma variável.
+	
+	// 6. Pesquise e descreva três principais características entre a biblioteca Swing, a biblioteca AWT (Abstract Window Toolkit) e o JavaFX?
+	// R: Todos realizam a criação de interfaces gráficas, possuem posicionamento de componentes na tela independente da resolução da tela e, por fim, +não achei ainda+.
+	
+	// 7. Pesquise o conceito de “Look and Feel” dentro do contexto de interfaces gráficas com Swing no Java. Procure alguns exemplos de temas, como o tema FlatLaf (https://github.com/JFormDesigner/FlatLaf).
+	// R: ...
 }
