c3774a73b6ac140fb6e6361f956a5879493d18c9
Emily Joanna Alves <88635006+emilyalvese@users.noreply.github.com>
2023-03-08 16:02:07 -0300

    Update Exercicio2.java

--- lista1/src/lista1/Exercicio2.java
+++ lista1/src/lista1/Exercicio2.java
@@ -1,9 +1,19 @@
 package lista1;
 
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {=
-
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		for(int i=0; i<10; i++){
+			String elemento = JOptionPane.showInputDialog("Insira uma informação: ");
+			lista.add(elemento);
+		}
+						
+		JOptionPane.showMessageDialog(null, lista);
 	}
 
 }
